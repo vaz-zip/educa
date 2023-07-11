f91d82fa71d762959904827ece73d1935ffc2f06
vaz-zip <baldeha@gmail.com>
2023-07-11 13:57:04 +0400

    'create app Students'

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -12,7 +12,7 @@ from django.apps import apps
 from django.db.models import Count
 from django.core.cache import cache
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-from students.forms import CourseEnrollForm
+# from students.forms import CourseEnrollForm
 from .forms import ModuleFormSet
 from .models import Course
 from .models import Module, Content
@@ -196,26 +196,19 @@ class ContentOrderView(CsrfExemptMixin,
 class CourseListView(TemplateResponseMixin, View):
     model = Course
     template_name = 'courses/course/list.html'
+    
     def get(self, request, subject=None):
-        subjects = cache.get('all_subjects')
-        if not subjects:
-            subjects = Subject.objects.annotate(
-                            total_courses=Count('courses'))
-            cache.set('all_subjects', subjects)
-        all_courses = Course.objects.annotate(
-                        total_modules=Count('modules'))
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
-            key = f'subject_{subject.id}_courses'
-            courses = cache.get(key)
-            if not courses:
-                courses = all_courses.filter(subject=subject)
-                cache.set(key, courses)
-        else:
-            courses = cache.get('all_courses')
-            if not courses:
-                courses = all_courses
-                cache.set('all_courses', courses)
+            courses = courses.filter(subject=subject)
         return self.render_to_response({'subjects': subjects,
-                                        'subject': subject,
-                                        'courses': courses})
+                                            'subject': subject,
+                                            'courses': courses})
+
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
+
 
 
 urlpatterns = [
@@ -28,6 +30,8 @@ urlpatterns = [
           name='logout'),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
+
 ]
 
 if settings.DEBUG:
