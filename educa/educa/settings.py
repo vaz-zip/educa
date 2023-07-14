860855947523bb50b4933507810185b29cba57da
vaz-zip <baldeha@gmail.com>
2023-07-14 19:22:56 +0400

    'add serializer'

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
     ]
 
 MIDDLEWARE = [
@@ -162,6 +163,14 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 INTERNAL_IPS = [
  '127.0.0.1',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+    }
+
